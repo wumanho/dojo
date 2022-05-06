878f6bcaa4ad34822d599e8c43886c4dcf60cd12
WUMANHO <wumanho@tutanota.com>
2022-05-06 22:57:06 +0800

    commit tupleObj

--- challenges/type-challenges/tupleToObj_easy/template.ts
+++ challenges/type-challenges/tupleToObj_easy/template.ts
@@ -1,1 +1,15 @@
-type TupleToObject<T extends readonly any[]> = any
+/**
+ * ts 遍历数组： in T[number]
+ */
+type TupleToObject<T extends readonly string[]> = {
+    [F in T[number]]: F
+}
+
+// js
+function tupleObject(arr) {
+    const obj = {}
+    arr.forEach(val => {
+        obj[val] = val
+    })
+    return obj
+}
--- challenges/type-challenges/tupleToObj_easy/test-cases.ts
+++ challenges/type-challenges/tupleToObj_easy/test-cases.ts
@@ -1,5 +1,6 @@
 import type {Equal, Expect} from '@type-challenges/utils'
 
+// 在 ts 中 as const = '字面量类型'，跟 js 的 const 有一定区别
 const tuple = ['tesla', 'model 3', 'model X', 'model Y'] as const
 
 type r = typeof tuple
