d96394bb3a4b8208469a4c4160df53650ba1c722
WUMANHO <wumanho@tutanota.com>
2022-07-31 20:45:29 +0800

    interview practice day 6

--- playground/functional.js
+++ playground/functional.js
@@ -4,10 +4,11 @@ function icall(thisArg, ...rest) {
   // step 1 兼容非对象参数和空值
   thisArg = thisArg ? Object(thisArg) : window
   // step 2 调用这个函数，绑定 this，记录返回值
-  thisArg.fn = this
-  const result = thisArg.fn(...rest)
+  const sy = Symbol()
+  thisArg[sy] = this
+  const result = thisArg[sy](...rest)
   // step 3 调用完成之后，删除这个 fn 属性
-  delete thisArg.fn
+  delete thisArg[sy]
   // step 4 返回结果
   return result
 }
@@ -20,6 +21,7 @@ function foo() {
 }
 
 function sum(num1, num2) {
+  console.log('sum', this)
   return num1 + num2
 }
 
@@ -27,15 +29,18 @@ function sum(num1, num2) {
 //   name: 'hi'
 // })
 
+// console.log(sum.icall({a: 'a'}, 2, 3))
+
 /** apply **/
 function iapply(thisArg, argArray = []) {
   // 判空
   thisArg = thisArg ? Object(thisArg) : window
   // apply
-  thisArg.fn = this
-  const result = thisArg.fn(...argArray)
+  const sy = Symbol()
+  thisArg[sy] = this
+  const result = thisArg[sy](...argArray)
   // delete
-  delete thisArg.fn
+  delete thisArg[sy]
   return result
 }
 
@@ -54,12 +59,13 @@ function applySum(num1, num2) {
 function ibind(thisArg, ...argArray) {
   // 判空
   thisArg = thisArg ? Object(thisArg) : window
-  thisArg.fn = this
+  const sy = Symbol()
+  thisArg[sy] = this
   return function bindFn(...args) {
     // 拼接参数
     const finalArray = argArray.concat(...args)
-    const result = thisArg.fn(...finalArray)
-    delete thisArg.fn
+    const result = thisArg[sy](...finalArray)
+    delete thisArg[sy]
     return result
   }
 }
@@ -74,6 +80,7 @@ function bindSum(num1, num2, num3, num4) {
 }
 
 const newFn = bindSum.ibind({name: 'bb'}, 20, 30, 40, 50)
+console.log(newFn())
 
 /** pure function **/
 
@@ -104,7 +111,7 @@ function pureSample1(info) {
   }
 }
 
-console.log(pureSample1(pureObj))
+// console.log(pureSample1(pureObj))
 
 
 
